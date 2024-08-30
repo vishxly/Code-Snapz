87748404bf51de82e0fae5821b51a153e1f8ab9d
Vishal Yadav <vishalyxo@gmail.com>
2024-08-30 21:40:35 +0530

    Added Footer

--- app/selectors/Footer.tsx
+++ app/selectors/Footer.tsx
@@ -9,16 +9,16 @@ function Footer() {
     <div className="flex items-center gap-10 py-16 dark:bg-black dark:text-white ">
       <Link
         className="text-sm font-medium flex gap-2 items-center   hover:text-slate-200 ease-in-out transition-all duration-150"
-        href="https://twitter.com/Sankalp2114"
+        href="https://twitter.com/vishxly"
       >
         Twitter <ExternalLink />
       </Link>
       <p className="text-sm font-medium flex gap-2 items-center">
-        Made by Sankalp
+        Made by Vyan
       </p>
       <Link
         className="text-sm font-medium flex gap-2 items-center   hover:text-slate-200 ease-in-out transition-all duration-150"
-        href="https://github.com/Sankalp2114/codesnap"
+        href="https://github.com/vishxly/Code-Snapz"
       >
         Source code <ExternalLink />
       </Link>
